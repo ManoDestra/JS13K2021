72b585276fe1dedfdc40fa3c42df8372bd3367c6
ManoDestra <manodestra.pl@gmail.com>
2021-08-22 15:19:40 -0500

    Started some random stars code.

--- assets/js/Space.js
+++ assets/js/Space.js
@@ -12,23 +12,43 @@ window.onresize = e => handleResize();
 
 const context = canvas.getContext('2d');
 
-const COLOR_BG = '#111';
 const COLOR_FG = '#0a0';
-const FONT = '1.2em Segoe UI';
+const DARK = '#111';
+const GREEN = '#0a0';
+const LIGHT = '#eee';
+const FONT = '4em Segoe UI';
 
 Nucleus.KeyInputHandler.start();
 
-const SpaceGame = {
-	loop(instant) {
-		context.fillStyle = COLOR_BG;
+const SpaceGame = (() => {
+	const COUNT = 250;
+
+	function randomStars() {
+		for (let i = 0; i < COUNT; i++) {
+			const x = parseInt(Math.random() * canvas.width);
+			const y = parseInt(Math.random() * canvas.height);
+			context.fillStyle = LIGHT;
+			context.fillRect(x, y, 2, 2);
+		}
+	}
+
+	function loop(instant) {
+		context.fillStyle = DARK;
 		context.fillRect(0, 0, canvas.width, canvas.height);
 
-		context.fillStyle = COLOR_FG;
+		context.fillStyle = GREEN;
 		context.font = FONT;
 		//context.fillText('Frame: ' + instant.frame, 50, 50);
-		context.fillText('FPS: ' + instant.fps().toFixed(3), 50, 50);
-		context.fillText('Random: ' + Math.random(), 50, 100);
+		context.fillText('FPS: ' + instant.fps().toFixed(3), 20, 100);
+		//context.fillText('Random: ' + Math.random(), 50, 100);
+
+		randomStars();
 	}
-};
+
+	return {
+		randomStars,
+		loop
+	};
+})();
 
 Nucleus.Clock.start(SpaceGame.loop);
