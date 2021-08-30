ce2fda521ea55c35e42a1481fd9b22f1d029344d
ManoDestra <manodestra.pl@gmail.com>
2021-08-29 22:49:17 -0500

    Some changes to Pure JS re instance variables. Still not working as expected with Nucleus.Clock.

--- assets/js/Pure.js
+++ assets/js/Pure.js
@@ -30,17 +30,29 @@ const Pure = (() => {
 	}
 
 	class Manager extends RenderComponent {
+		// TODO: this should be a private instance variable. Maybe use a module for Pure?
+		#components = [];
+
 		constructor() {
 			super();
-			this.components = [];
+		}
+
+		add(component) {
+			if (component instanceof Component) {
+				this.#components.push(component);
+			}
 		}
 
 		update(instant) {
-			this.components.filter(c => c instanceof Component).forEach(c => c.update(instant));
+			this.#components.filter(c => c instanceof Component).forEach(c => c.update(instant));
 		}
 
 		render(instant) {
-			this.components.filter(c => c instanceof RenderComponent).forEach(c => c.render(instant));
+			this.#components.filter(c => c instanceof RenderComponent).forEach(c => c.render(instant));
+		}
+
+		static instance() {
+			return new Manager();
 		}
 	}
 
