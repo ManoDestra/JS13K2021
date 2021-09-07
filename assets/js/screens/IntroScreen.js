27c3c76e01623cf88c9b53de278ef1611d638eee
ManoDestra <manodestra.pl@gmail.com>
2021-09-07 07:49:53 -0500

    Started IntroScreen.

--- assets/js/screens/IntroScreen.js
+++ assets/js/screens/IntroScreen.js
@@ -1,4 +1,6 @@
 class IntroScreen extends Urge.Screen {
+	#totalElapsed = 0;
+
 	constructor(game, state) {
 		super(game, state);
 	}
@@ -8,9 +10,17 @@ class IntroScreen extends Urge.Screen {
 	}
 
 	update(instant) {
+		this.#totalElapsed += instant.elapsed();
+		this.debug(instant, this.#totalElapsed);
 	}
 
 	render(instant) {
+		const ctx = this.getContext();
+		ctx.lineWidth = 2;
+		ctx.strokeStyle = 'white';
+		ctx.fillStyle = 'darkred';
+		ctx.strokeRect(100, 100, 100, 100);
+		ctx.fillRect(100, 100, 100, 100);
 	}
 
 	term() {
