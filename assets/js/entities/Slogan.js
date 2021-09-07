dc2e8d85497f1eafcc07274bce8ee5fe316c4e7a
ManoDestra <manodestra.pl@gmail.com>
2021-09-07 08:11:55 -0500

    Started slogan component.

---
+++ assets/js/entities/Slogan.js
@@ -0,0 +1,13 @@
+class Slogan extends Urge.Sprite {
+	constructor(context) {
+		super(context);
+	}
+
+	update(instant) {
+		super.update(instant);
+	}
+
+	render(instant) {
+		super.render(instant);
+	}
+}
