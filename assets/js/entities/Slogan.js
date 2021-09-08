51ccbbfe62e950c783f68e52ba664325b10d2b97
ManoDestra <manodestra.pl@gmail.com>
2021-09-08 05:15:16 -0500

    Started adding Slogan rendering, but only the last defined Slogan is updating/rendering correctly.

--- assets/js/entities/Slogan.js
+++ assets/js/entities/Slogan.js
@@ -1,16 +1,38 @@
 class Slogan extends Urge.Sprite {
 	#text;
+	#delay;
+	#totalElapsed = 0;
 
-	constructor(context, text) {
-		super(context);
+	constructor(context, x, y, width, height, text, delay) {
+		super(context, x, y, width, height);
 		this.#text = text;
+		this.#delay = delay;
 	}
 
 	update(instant) {
 		super.update(instant);
+		if (this.#totalElapsed > this.#delay) {
+			const velocity = -0.5;
+			const delta = this.getHeight() * velocity * (instant.elapsed() / 1000);
+			this.offsetY(delta);
+		}
+
+		this.#totalElapsed += instant.elapsed();
 	}
 
 	render(instant) {
 		super.render(instant);
+		if (this.#totalElapsed > this.#delay) {
+			//this.debug(instant, this.#text, this.#delay, this.#totalElapsed);
+
+			const ctx = this.getContext();
+			const canvas = this.getCanvas();
+
+			ctx.strokeStyle = 'darkgray';
+			ctx.fillStyle = 'white';
+			ctx.font = (this.getHeight() / 4) + 'px sans-serif';
+			ctx.textAlign = 'center';
+			ctx.fillText(this.#text, this.getX(), this.getY());
+		}
 	}
 }
--- assets/js/screens/IntroScreen.js
+++ assets/js/screens/IntroScreen.js
@@ -16,27 +16,54 @@ class IntroScreen extends Urge.Screen {
 		'Use W|S|A|D To Move, Space To Fire',
 		'Now, Get Out There And Save Our Race From Infection!'
 	];
+	#spawned;
 
 	constructor(game, state) {
 		super(game, state);
+		this.#spawned = this.#lines.map(e => false);
+		console.log(this.#spawned);
 	}
 
 	init() {
 		super.init();
+		const store = this.getStore();
+		const canvas = this.getCanvas();
+		const ctx = this.getContext();
+		const x = canvas.width / 2;
+		const y = canvas.height * 0.9;
+		const width = canvas.width / 3;
+		const height = canvas.height / 5;
+		for (let i = 0; i < this.#lines.length; i++) {
+			const text = this.#lines[i];
+			const delay = i * 1000;
+			const slogan = new Slogan(ctx, x, y, width, height, text, delay);
+			store.put(slogan);
+		}
+
+		store.forEach(c => {
+			console.log('Component:', c);
+		});
 	}
 
 	update(instant) {
 		const store = this.getStore();
+		store.update(instant);
 		this.#totalElapsed += instant.elapsed();
 	}
 
 	render(instant) {
 		const ctx = this.getContext();
+		const store = this.getStore();
+		store.render(instant);
+		//store.renderByTypes(instant, Slogan);
+
+		/*
 		ctx.lineWidth = 2;
 		ctx.strokeStyle = 'white';
 		ctx.fillStyle = 'darkred';
 		ctx.strokeRect(100, 100, 100, 100);
 		ctx.fillRect(100, 100, 100, 100);
+		*/
 	}
 
 	term() {
