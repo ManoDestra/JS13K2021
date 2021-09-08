aced350b747be14da94d65f4de9cf7658a291b18
Peter Lamb <plamb@csa1.com>
2021-09-08 15:16:50 -0500

    Fixed build scripts.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
-del *.zip
+del Rogue-19.zip
 7z a Rogue-19.zip index.html assets
 @echo on
--- build_min.cmd
+++ build_min.cmd
@@ -1,4 +1,4 @@
 @echo off
-del *.zip
-7z a Rogue-19.zip index.html assets\css assets\js\*.min.js assets\js\entities\*.min.js assets\js\screens\*.min.js
+del Rogue-19.min.zip
+7z a Rogue-19.min.zip index_min.html assets\css assets\js\*.min.js assets\js\entities\*.min.js assets\js\screens\*.min.js
 @echo on
