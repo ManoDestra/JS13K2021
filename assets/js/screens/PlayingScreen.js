5ca2cb418dab4df0af73e2691dafb24678e8aa84
ManoDestra <manodestra.pl@gmail.com>
2021-09-09 04:16:04 -0500

    Got starfields rendering on the new PlayingScreen.

--- assets/js/screens/PlayingScreen.js
+++ assets/js/screens/PlayingScreen.js
@@ -5,12 +5,33 @@ class PlayingScreen extends Urge.Screen {
 
 	init() {
 		super.init();
+		const state = this.getState();
+		const store = this.getStore();
+		const ctx = this.getContext();
+		const sf1 = new StarField(ctx, {
+			image: state.assets.starFields[0],
+			scrollSeconds: 30
+		});
+		const sf2 = new StarField(ctx, {
+			image: state.assets.starFields[1],
+			scrollSeconds: 24
+		});
+		const sf3 = new StarField(ctx, {
+			image: state.assets.starFields[2],
+			scrollSeconds: 21
+		});
+		store.put(sf1, sf2, sf3);
 	}
 
 	update(instant) {
+		const store = this.getStore();
+		store.update(instant);
 	}
 
 	render(instant) {
+		super.render(instant);
+		const store = this.getStore();
+		store.render(instant);
 	}
 
 	term() {
