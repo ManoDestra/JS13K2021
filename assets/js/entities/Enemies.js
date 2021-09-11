8f7701aad09e57f2ad233227effa683cda476301
ManoDestra <manodestra.pl@gmail.com>
2021-09-10 20:40:11 -0500

    Improved the cell rendering, but I can't get the curve way working right now. It's not performant.

--- assets/js/entities/Enemies.js
+++ assets/js/entities/Enemies.js
@@ -31,9 +31,8 @@ class Cell extends Enemy {
 
 	constructor(context, x, y, size, health, velocity) {
 		super(context, x, y, size, size, health, velocity);
-		const center = [200, 200];
-		const minRadius = 40;
-		const maxRadius = 70;
+		const minRadius = this.size / 3;
+		const maxRadius = this.size / 2;
 		const count = 18;
 		for (let i = 0; i < count; i++) {
 			const angle = Math.PI * 2 * i / count;
@@ -64,5 +63,37 @@ class Cell extends Enemy {
 		ctx.stroke();
 		ctx.fill();
 		ctx.closePath();
+
+		ctx.lineWidth = 3;
+		ctx.strokeStyle = 'red';
+		ctx.fillStyle = 'blue';
+		ctx.beginPath();
+		ctx.arc(center[0], center[1], this.getWidth() / 6, 0, Math.PI * 2);
+		ctx.stroke();
+		ctx.fill();
+		ctx.closePath();
+
+		/*
+		ctx.beginPath();
+		ctx.strokeStyle = 'white';
+		ctx.moveTo(this.getX() + (this.getWidth() / 2) + this.#points[0][0], this.getY() + (this.getHeight() / 2) + this.#points[0][1]);
+		for (let i = 0; i < this.#points.length; i++) {
+			const nextIndex = (i + 1) % this.#points.length;
+			const x = this.getX() + (this.getWidth() / 2) + this.#points[i][0];
+			const y = this.getY() + (this.getHeight() / 2) + this.#points[i][1];
+			const nextX = this.getX() + (this.getWidth() / 2) + this.#points[nextIndex][0];
+			const nextY = this.getY() + (this.getHeight() / 2) + this.#points[nextIndex][1];
+
+			const xMid = (x + nextX) / 2;
+			const yMid = (y + nextY) / 2;
+			const cpx1 = (xMid + x) / 2;
+			const cpx2 = (xMid + nextX) / 2;
+			ctx.quadraticCurveTo(cpx1, y, xMid, yMid);
+			ctx.quadraticCurveTo(cpx2, nextY, nextX, nextY);
+		}
+
+		ctx.closePath();
+		ctx.stroke();
+		*/
 	}
 }
