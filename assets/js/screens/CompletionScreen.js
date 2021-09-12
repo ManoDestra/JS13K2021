c125328355d4cd45647b30473948db41e5211806
ManoDestra <manodestra.pl@gmail.com>
2021-09-11 19:51:33 -0500

    Added basic game over and completion logic, so that the game kind of works as expected. The IntroScreen isn't quite right re init(), but easily fixed.

--- assets/js/screens/CompletionScreen.js
+++ assets/js/screens/CompletionScreen.js
@@ -1,5 +1,29 @@
 class CompletionScreen extends Urge.Screen {
+	#totalElapsed = 0;
+
 	constructor(game, state) {
 		super(game, state);
+	}
+
+	init() {
+		this.#totalElapsed = 0;
+	}
+
+	update(instant) {
+		this.#totalElapsed += instant.elapsed();
+		if (this.#totalElapsed > 5000 && this.getScreenState() == Urge.ScreenState.ACTIVE) {
+			this.navigate(StartScreen);
+		}
+	}
+
+	render(instant) {
+		super.render(instant);
+
+		const canvas = this.getCanvas();
+		const ctx = this.getContext();
+		ctx.font = '32px sans-serif';
+		ctx.strokeStyle = 'white';
+		ctx.textAlign = 'center';
+		ctx.strokeText('Congratulations!', canvas.width / 2, canvas.height / 2);
 	}
 }
