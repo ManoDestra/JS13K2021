b5924e31ae5215634c21093939ef4d12a692624a
ManoDestra <manodestra.pl@gmail.com>
2021-09-22 05:18:27 -0500

    Amended code back after relative rendering test.

--- ogre/assets/js/Monoculture.js
+++ ogre/assets/js/Monoculture.js
@@ -16,31 +16,33 @@ class Monoculture extends Game {
 		// TODO: this call could be automatic
 		this.clearCanvas(canvas, ctx);
 
-		//const widthUnit = canvas.width / 10;
-		//const heightUnit = canvas.height / 10;
-		//const width = widthUnit * 8;;
-		//const height = heightUnit * 8;
+		const widthUnit = canvas.width / 10;
+		const heightUnit = canvas.height / 10;
+		const width = widthUnit * 8;
+		const height = heightUnit * 8;
 
 		ctx.save();
 
-		ctx.lineWidth = 0.02;
+		//ctx.lineWidth = 0.02;
+		ctx.lineWidth = 5;
 		ctx.lineCap = 'round';
 		ctx.lineJoin = 'round';
 		ctx.strokeStyle = 'white';
 		ctx.fillStyle = 'darkred';
-		ctx.translate(20, 30);
-		ctx.scale(canvas.width, canvas.height);
-		if (!this.#transform) {
-			this.#transform = ctx.getTransform();
-			console.log(this.#transform);
-		}
+		//ctx.translate(20, 30);
+		//ctx.scale(canvas.width, canvas.height);
 
-		ctx.strokeRect(0.1, 0.1, 0.8, 0.8);
-		ctx.fillRect(0.1, 0.1, 0.8, 0.8);
+		//ctx.strokeRect(0.1, 0.1, 0.8, 0.8);
+		//ctx.fillRect(0.1, 0.1, 0.8, 0.8);
 
 		//ctx.globalAlpha = 0.7;
-		//ctx.strokeRect(widthUnit, heightUnit, width, height);
-		//ctx.fillRect(widthUnit, heightUnit, width, height);
+		ctx.strokeRect(widthUnit, heightUnit, width, height);
+		ctx.fillRect(widthUnit, heightUnit, width, height);
+
+		ctx.textAlign = 'center';
+		ctx.font = '48px Segoe UI';
+		ctx.lineWidth = 1.5;
+		ctx.strokeText('FPS: ' + Math.floor(instant.fps()), canvas.width / 2, canvas.height / 2);
 
 		ctx.restore();
 	}
@@ -51,6 +53,18 @@ window.addEventListener('load', async () => {
 	await game.init();
 	game.start();
 
-	const matrix = new DOMMatrix([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+	console.log(DOMPoint);
+	console.log(DOMRect);
+	console.log(DOMQuad);
+	console.log(DOMMatrix);
+
+	const point = new DOMPoint(3, 4, 5, 1);
+	console.log(point);
+	const matrix = new DOMMatrix([2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1]);
 	console.log(matrix);
+	console.log(matrix.toFloat32Array());
+	console.log(matrix.toFloat64Array());
+	const transformed = point.matrixTransform(matrix);
+	console.log(transformed);
+	console.log(point);
 });
