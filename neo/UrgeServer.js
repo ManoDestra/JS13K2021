9c6bfa79eb2c237ec558ba7f9f36fcddb145be91
ManoDestra <manodestra.pl@gmail.com>
2023-11-25 20:01:07 -0600

    Refactor.

--- neo/UrgeServer.js
+++ neo/UrgeServer.js
@@ -1,5 +1,7 @@
 importScripts(`UrgeCore.js?r=${Math.random()}`);
 
+let server = null;
+
 class UrgeServer {
 	#ctx;
 	#active = true;
@@ -49,105 +51,107 @@ class UrgeServer {
 	}
 }
 
-let server = null;
+class Handler {
+	static onMessage(e) {
+		const { type, request } = e.data;
 
-self.onmessage = e => {
-	const { type, request } = e.data;
-	//console.log('Client Message:', e.data);
-	//console.log('Client Message [Breakdown]:', type, request);
+		switch (type) {
+			case 'IMPORT':
+				const reqToUse = Array.isArray(request) ? request : [request];
+				importScripts(...request);
+				break;
+			case 'INIT':
+				// TODO: handle assets or just initialize bespoke game class?
 
-	switch (type) {
-		case 'IMPORT':
-			const reqToUse = Array.isArray(request) ? request : [request];
-			importScripts(...request);
-			break;
-		case 'INIT':
-			// TODO: handle assets or just initialize bespoke game class?
-
-			const gameClass = eval(request);
-			if (!gameClass) {
-				throw new Error('Class Not Found: ' + request);
-			}
-
-			// TODO: fix interface, probably gameClass.start() or gameClass.init().
-			const game = new gameClass();
-			game.process();
-			break;
-		case 'START':
-			server = new UrgeServer(request);
-			server.start();
-			break;
-		case 'RESIZE':
-			server.resize(request);
-			break;
-		case 'VISIBILITY':
-			server.active = request;
-			break;
-		case 'KEYBOARD':
-			break;
-		case 'GAME_PAD':
-			break;
-		default:
-			throw new Error('Unsupported Message Type: ' + type);
-	}
-
-	//postMessage(`${type}: Complete ${performance.now()}`);
-
-	/*
-	if (type === 'IMG') {
-		console.log(request.width + 'x' + request.height);
-		createImageBitmap(request).then(img => {
-			console.log('IMG:', img, img.width, img.height);
-		});
-	}
-	*/
-
-	/*
-	switch (type) {
-		case 'START':
-			postMessage({
-				type: 'START',
-				request,
-				response: {
-					message: 'The game has been started'
+				const gameClass = eval(request);
+				if (!gameClass) {
+					throw new Error('Class Not Found: ' + request);
 				}
+
+				// TODO: fix interface, probably gameClass.start() or gameClass.init().
+				const game = new gameClass();
+				game.process();
+				break;
+			case 'START':
+				server = new UrgeServer(request);
+				server.start();
+				break;
+			case 'RESIZE':
+				server.resize(request);
+				break;
+			case 'VISIBILITY':
+				server.active = request;
+				break;
+			case 'KEYBOARD':
+				break;
+			case 'GAME_PAD':
+				break;
+			default:
+				throw new Error('Unsupported Message Type: ' + type);
+		}
+
+		//postMessage(`${type}: Complete ${performance.now()}`);
+
+		/*
+		if (type === 'IMG') {
+			console.log(request.width + 'x' + request.height);
+			createImageBitmap(request).then(img => {
+				console.log('IMG:', img, img.width, img.height);
 			});
-			break;
-		case 'IMPORT':
-			const reqToUse = Array.isArray(request) ? request : [request];
-			importScripts(...request);
-			postMessage('IMPORTED');
-			break;
-		case 'INIT':
-			for (let c of request) {
-				if (['.', ';', ' '].includes(c)) {
-					throw new Error('Invalid Character: ' + c);
+		}
+		*/
+
+		/*
+		switch (type) {
+			case 'START':
+				postMessage({
+					type: 'START',
+					request,
+					response: {
+						message: 'The game has been started'
+					}
+				});
+				break;
+			case 'IMPORT':
+				const reqToUse = Array.isArray(request) ? request : [request];
+				importScripts(...request);
+				postMessage('IMPORTED');
+				break;
+			case 'INIT':
+				for (let c of request) {
+					if (['.', ';', ' '].includes(c)) {
+						throw new Error('Invalid Character: ' + c);
+					}
 				}
-			}
 
-			// Normally, eval should be avoided, but there's no way round it here, hence the above validation.
-			const gameClass = eval(request);
-			if (!gameClass) {
-				throw new Error('Class Not Found: ' + request);
-			}
+				// Normally, eval should be avoided, but there's no way round it here, hence the above validation.
+				const gameClass = eval(request);
+				if (!gameClass) {
+					throw new Error('Class Not Found: ' + request);
+				}
 
-			// TODO: fix interface, probably gameClass.start() or gameClass.init().
-			const game = new gameClass();
-			game.process();
+				// TODO: fix interface, probably gameClass.start() or gameClass.init().
+				const game = new gameClass();
+				game.process();
 
-			postMessage('INITIALIZED');
+				postMessage('INITIALIZED');
 
-			break;
-		default:
-			throw new Error(`Unsupported Type: ${type}`);
+				break;
+			default:
+				throw new Error(`Unsupported Type: ${type}`);
+		}
+		*/
 	}
-	*/
-};
 
-self.onmessageerror = e => {
-	console.error('Message Error:', e);
-};
+	static onMessageError() {
+		console.error('Server Message Error:', e);
+	}
 
-self.onerror = e => {
-	console.error('Worker Error:', e);
-};
+	static onError(e) {
+		console.error('Server Error:', e);
+	}
+}
+
+self.onmessage = Handler.onMessage;
+self.onmessageerror = Handler.onMessageError;
+self.onerror = Handler.onError;
