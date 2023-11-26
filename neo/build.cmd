507bbb3862dfde83a26852cd70c1303704f81d0d
ManoDestra <manodestra.pl@gmail.com>
2023-11-26 00:47:16 -0600

    Updated build script.

--- neo/build.cmd
+++ neo/build.cmd
@@ -1,4 +1,4 @@
 @echo off
 del Rogue-19.zip
-7z a Rogue-19.zip index.html ..\favicon.bmp UrgeCore.js UrgeClient.js UrgeServer.js Rogue19Client.js
+7z a Rogue-19.zip index.html ..\favicon.bmp UrgeCore.js UrgeClient.js UrgeServer.js Rogue19Client.js Rogue19Server.js
 @echo on
