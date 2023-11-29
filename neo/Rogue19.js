883cebeacc79ff7379963351239ca2cba3c852af
ManoDestra <manodestra.pl@gmail.com>
2023-11-29 00:06:12 -0600

    Experimenting with filters.

--- neo/Rogue19.js
+++ neo/Rogue19.js
@@ -5,10 +5,11 @@ class Rogue19 extends UrgeGame {
 	render() {
 		super.render();
 
-		const opacity = 100;
+		//const opacity = 100;
+		const value = 100 - parseInt(GameTime.current() / 100);
 		Object.assign(this.ctx, {
 			font: 'bold 48px Segoe UI',
-			strokeStyle: 'white',
+			strokeStyle: 'cornflowerblue',
 			lineWidth: 2,
 			//lineJoin: 'round',
 			shadowColor: 'silver',
@@ -18,7 +19,7 @@ class Rogue19 extends UrgeGame {
 			fillStyle: 'white',
 			textAlign: 'left',
 			textBaseline: 'top',
-			filter: `opacity(${opacity}%)`
+			filter: `sepia(${value}%)`
 		});
 
 		if (GameTime.secondsChanged()) {
@@ -27,7 +28,7 @@ class Rogue19 extends UrgeGame {
 
 		const text = `FPS: ${GameTime.fpsAsInt()} - ${GameTime.currentSeconds()} Seconds`;
 		const size = this.ctx.measureText(text);
-		const border = 16;
+		const border = 8;
 		this.ctx.strokeText(text, border, border);
 		//this.ctx.fillText(text, border, border);
 	}
